2a5b51d3693c81dd73f4d40bb4cdf7319cc4bde9
Martina <martina.pozzerle@libero.it>
2020-10-19 15:26:41 +0200

    #password

---
+++ script.js
@@ -0,0 +1,4 @@
+var nomeUtente = prompt("Qual è il tuo nome?");
+var cognomeUtente = prompt("Qual è il tuo cognome?");
+var colore = prompt("Qual è il tuo colore preferito?");
+document.getElementById("password").innerHTML = nomeUtente + cognomeUtente + colore + "19";
